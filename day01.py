a3361fa880e35b655c5196d61facbcfca068d6a4
Alex Tan Hong Pin <alextan220990@gmail.com>
2021-12-31 00:03:15 +0800

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,25 @@
+input = "R3, L5, R1, R2, L5, R2, R3, L2, L5, R5, L4, L3, R5, L1, R3, R4, R1, L3, R3, L2, L5, L2, R4, R5, R5, L4, L3, L3, R4, R4, R5, L5, L3, R2, R2, L3, L4, L5, R1, R3, L3, R2, L3, R5, L194, L2, L5, R2, R1, R1, L1, L5, L4, R4, R2, R2, L4, L1, R2, R53, R3, L5, R72, R2, L5, R3, L4, R187, L4, L5, L2, R1, R3, R5, L4, L4, R2, R5, L5, L4, L3, R5, L2, R1, R1, R4, L1, R2, L3, R5, L4, R2, L3, R1, L4, R4, L1, L2, R3, L1, L1, R4, R3, L4, R2, R5, L2, L3, L3, L1, R3, R5, R2, R3, R1, R2, L1, L4, L5, L2, R4, R5, L2, R4, R4, L3, R2, R1, L4, R3, L3, L4, L3, L1, R3, L2, R2, L4, L4, L5, R3, R5, R3, L2, R5, L2, L1, L5, L1, R2, R4, L5, R2, L4, L5, L4, L5, L2, L5, L4, R5, R3, R2, R2, L3, R3, L2, L5"
+
+dir = 0 + 1j # North
+pos = 0 + 0j
+cache = {}
+found = False
+
+def manhattan_distance(num: complex):
+	return abs(num.imag) + abs(num.real)
+
+for step in input.split(", "):
+  d = step[0]
+  dist = int(step[1:])
+  
+  dir *= -1j if d == "R" else 1j
+  for _ in range(dist):
+    pos += 1 * dir
+    if pos in cache:
+      if not found:
+	      print(f"part 2: {manhattan_distance(pos)}")
+      found = True
+    else:
+      cache[pos] = True
+
+print(f"part 1: {manhattan_distance(pos)}")
